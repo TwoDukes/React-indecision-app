a557be8931a9deb3022c77523eac95833105dbe0
Unknown <magicdustin14@gmail.com>
2017-11-11 15:37:20 -0800

    setup basic rendering of options array

--- src/app.js
+++ src/app.js
@@ -1,51 +1,72 @@
+class IndecisionApp extends React.Component {
+  render() {
+    const title = "Indecision";
+    const subtitle = "Put your life in the hands of a computer";
+    const options = ['thing one', 'thing two', 'thing four'];
+
+    return (
+      <div>
+        <Header title={title} subtitle={subtitle}/>
+        <Action/>
+        <Options options={options}/>
+        <AddOption/>
+      </div>
+    );
+  }
+}
+
 class Header extends React.Component {
-    render(){
-        return (
-            <div>
-                <h1>Indecision</h1>
-                <h2>Put your life in the hands of a computer</h2>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title || 'No Title'}</h1>
+        <h2>{this.props.subtitle || 'No Subtitle'}</h2>
+      </div>
+    );
+  }
 }
 
 class Action extends React.Component {
-    render(){
-        return (
-            <div>
-                <button>What should I do?</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <button>What should I do?</button>
+      </div>
+    );
+  }
 }
 
 class Options extends React.Component {
-    render(){
-        return (
-            <div>
-                <p>Options component here</p>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        {
+          this.props.options.map((option)=> <Option key={option} optionText={option} />)
+        } 
+      </div>
+    );
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.optionText}
+      </div>
+    );
+  }
 }
 
 class AddOption extends React.Component {
-    render(){
-        return (
-            <div>
-                <p>AddOption component here</p>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        AddOption component here
+      </div>
+    );
+  }
 }
 
-const jsx = (
-    <div>
-        <Header/>
-        <Action/>
-        <Options/>
-        <AddOption/>
-    </div>
-);
-
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(
+  <IndecisionApp/>, document.getElementById('app'));
