feac2ac6202e2f564c172fb3e2ba7b597ee5808e
Unknown <magicdustin14@gmail.com>
2017-11-13 21:07:29 -0800

    some general styling to the page look

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -81,18 +81,22 @@ export default class IndecisionApp extends React.Component {
     return (
       <div>
         <Header subtitle={subtitle}/>
-        <Action 
+
+        <div className='container'>
+          <Action 
           handlePick={this.handlePick}
           hasOption={this.state.options.length > 0}
-        />
-        <Options 
-          options={this.state.options}
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption
-          handleAddOption={this.handleAddOption}
-        />
+          />
+          <Options 
+            options={this.state.options}
+            handleDeleteOptions={this.handleDeleteOptions}
+            handleDeleteOption={this.handleDeleteOption}
+          />
+          <AddOption
+            handleAddOption={this.handleAddOption}
+          />
+        </div>
+        
         <OptionModal
           selectedOption={this.state.selectedOption}
           handleClearSelectedOption={this.handleClearSelectedOption}
