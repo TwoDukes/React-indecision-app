632e3918b96f828ff4d8c0dd5341ca260f4d38b9
Unknown <magicdustin14@gmail.com>
2017-11-13 19:45:38 -0800

    refactored the stateless functional components to implicitly return

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
 //The what should I do button
-const Action = (props) => {
-  return (
-      <div>
-        <button 
-          onClick={props.handlePick} 
-          disabled={!props.hasOption}
-        >
-          What should I do?
-        </button>
-      </div>
-    );
-}
+const Action = (props) => (
+  <div>
+    <button 
+      onClick={props.handlePick} 
+      disabled={!props.hasOption}
+    >
+      What should I do?
+    </button>
+  </div>
+);
 
 export default Action;
--- src/components/Options.js
+++ src/components/Options.js
@@ -2,22 +2,20 @@ import React from 'react';
 import Option from './Option';
 
 //The options list with remove all button
-const Options = (props) => {
-  return (
-      <div>
-      <button onClick={props.handleDeleteOptions}>Remove All</button>
-      {props.options.length === 0 && <p>Please add an option to get started!</p>}
-        {
-          props.options.map((option) => (
-              <Option 
-                key={option} 
-                optionText={option}
-                handleDeleteOption={props.handleDeleteOption}
-              />
-            ))
-        } 
-      </div>
-    );
-}
+const Options = (props) => (
+  <div>
+  <button onClick={props.handleDeleteOptions}>Remove All</button>
+  {props.options.length === 0 && <p>Please add an option to get started!</p>}
+    {
+      props.options.map((option) => (
+          <Option 
+            key={option} 
+            optionText={option}
+            handleDeleteOption={props.handleDeleteOption}
+          />
+        ))
+    } 
+  </div>
+);
 
 export default Options;
