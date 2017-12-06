b6c870cf919ba9ef6bb51952e2c5d36a87df314f
Unknown <magicdustin14@gmail.com>
2017-12-06 00:56:34 -0800

    Setup app for Heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,31 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader', //run babel
-      test: /\.js$/, //on all js files
-      exclude: /node_modules/ //except the ones in node modules
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'cheap-module-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public')
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader', //run babel
+        test: /\.js$/, //on all js files
+        exclude: /node_modules/ //except the ones in node modules
+      }, {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }]
+    },
+    devtool: isProduction ? 'source-map' :'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public')
+    }
   }
 };
 
